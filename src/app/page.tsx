ff486a216e22368aab3562b3ac1b396871e256e0
Vinícius Dias <diasvinicius0401@gmail.com>
2025-10-22 22:16:50 -0300

    feat: adiciona estrura do head e estiliza

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,29 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <main>
-        <section className="flex flex-col items-center justify-center text-center px-6 py-16 bg-gray-50">
-          <h1 className="text-4xl md:text-5xl font-extrabold mb-6 text-gray-900">
-            Bem-vindo ao GestorX
-          </h1>
-          <p className="text-lg md:text-xl mb-8 text-gray-700 max-w-2xl">
-            Simplifique a gestão do seu negócio com nossa plataforma intuitiva e
-            eficiente.
-          </p>
-          <Image
-            src="/assets/tasks.png"
-            alt="Imagem de Gestão"
-            width={800}
-            height={600}
-            className="w-full max-w-3xl mb-8 rounded-lg shadow-lg"
-          />
-          <a
-            href="#"
-            className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
-          >
-            Comece Agora
-          </a>
-        </section>
-      </main>
-    </div>
+    <main className="flex flex-col items-center justify-center text-center px-6 pt-8 pb-16 bg-black color-gray-100">
+      <h1 className="text-4xl md:text-5xl font-extrabold mb-6 text-gray-100">
+        Bem-vindo ao GestorX
+      </h1>
+      <p className="text-lg md:text-xl mb-4 text-gray-300 max-w-2xl">
+        Simplifique a gestão do seu negócio com nossa plataforma intuitiva e
+        eficiente.
+      </p>
+      <Image
+        src="/assets/tasks.png"
+        alt="Imagem de Gestão"
+        width={300}
+        height={100}
+        className="mb-4"
+      />
+      <section className="flex gap-5 items-center justify-center">
+        <div className="bg-slate-100 p-3 rounded-lg text-gray-900 hover:bg-orange-500 hover:text-white transition-colors duration-300 cursor-pointer hover:scale-105">
+          <span>+12 posts</span>
+        </div>
+        <div className="bg-slate-100 p-3 rounded-lg text-gray-900 hover:bg-orange-500 hover:text-white transition-colors duration-300 cursor-pointer hover:scale-105">
+          <span>+12 posts</span>
+        </div>
+      </section>
+    </main>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
 export default function Header() {
   return (
-    <header>
-      <h1>Header Component</h1>
+    <header className="bg-black w-full px-8 py-4">
+      <div className="w-full flex justify-between  md:max-w-[900px] mx-auto items-center">
+        <div>
+          <h1 className="font-bold text-white text-2xl md:text-3xl p-4">
+            TAREFAS<span className="text-orange-500">+</span>
+          </h1>
+        </div>
+        <div className="px-4 py-2 rounded-3xl text-white border border-white bg-none cursor-pointer transition-colors duration-300 hover:scale-105">
+          LOGIN
+        </div>
+      </div>
     </header>
   );
 }
