ed69e22fe233712b26b35a799a32bc5149db5d79
Vinícius Dias <diasvinicius0401@gmail.com>
2025-10-22 21:15:51 -0300

    feat: substituição do tailwind para a versão antiga e adaptação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Testando</h1>
-    </main>
+    <div>
+      <main>
+        <section className="flex flex-col items-center justify-center text-center px-6 py-16 bg-gray-50">
+          <h1 className="text-4xl md:text-5xl font-extrabold mb-6 text-gray-900">
+            Bem-vindo ao GestorX
+          </h1>
+          <p className="text-lg md:text-xl mb-8 text-gray-700 max-w-2xl">
+            Simplifique a gestão do seu negócio com nossa plataforma intuitiva e
+            eficiente.
+          </p>
+          <Image
+            src="/assets/tasks.png"
+            alt="Imagem de Gestão"
+            width={800}
+            height={600}
+            className="w-full max-w-3xl mb-8 rounded-lg shadow-lg"
+          />
+          <a
+            href="#"
+            className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+          >
+            Comece Agora
+          </a>
+        </section>
+      </main>
+    </div>
   );
 }
---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,7 @@
+export default function Sobre() {
+  return (
+    <main>
+      <h1>Sobre a aplicação</h1>
+    </main>
+  );
+}
